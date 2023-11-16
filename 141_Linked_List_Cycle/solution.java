80b2cfbb8d01a0652c24614e37c69674799ba0d5
Lester Tan <127372303+SunsetStories@users.noreply.github.com>
2023-11-16 13:35:28 +0800

    Add files via upload

---
+++ 141_Linked_List_Cycle/solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        /*
+            Input: Head of linked list
+            Output: boolean true if a cycle exist, false otherwise
+
+            Strategy
+            1.) fast and slow pointer
+                - if cycle exist, fast will be equal to slow
+                - else, fast will reach null
+        */
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
